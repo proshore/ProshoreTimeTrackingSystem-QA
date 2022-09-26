1448fa09a821db35228fdaff2492cf1d5afb125f
mahima <mahimabh93gmail.com>
2022-09-26 14:37:14 +0545

    add test spec for login  odule

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,29 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', () => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.get("#email").type("test@test.com");
+    cy.get("#password").type("test123").type("{enter}")
+    // cy.get('form > .d-grid').click()
+    // cy.get('[data-cy="loginButton"]').click()
+
+  })
+  Cypress.Commands.add('invalidLogin', (emailaddress, password) => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.get("#email").type(emailaddress);
+    cy.get("#password").type(password).type("{enter}")
+    // cy.get('form > .d-grid').click()
+    // cy.get("[type='submit']").click()
+
+  })
+  Cypress.Commands.add('project', (name, clientname) => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.get('[data-cy="projectsSidebarButton"]').contains("Projects").click();
+    cy.get('[data-cy="addProjectButton"]').click();
+    
+   
+  })
