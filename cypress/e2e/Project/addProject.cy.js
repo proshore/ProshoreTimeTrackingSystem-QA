ce2a40eba0b2c77709856b3c861ece2537cef846
mahima <mahimabh93gmail.com>
2022-10-11 16:51:28 +0545

    viewport

--- cypress/e2e/Project/addProject.cy.js
+++ cypress/e2e/Project/addProject.cy.js
@@ -1,26 +1,29 @@
 const { getRandomString } = require("../../../utilites/helper");
 const addProject_PO = require("../../page_objects/AddProject/addProject_PO");
 
-describe("Search in project module", function () {
-    beforeEach(function () {
-      cy.login();
-addProject_PO.visitProjectModulePage()
-    });
-    it("Should create a new project ", function () {
-    const randomGenerateName = getRandomString()
-    addProject_PO.projectInput(randomGenerateName)
-    addProject_PO.clientInput()
-    addProject_PO.createProject()    
-  })
-  it("click billable button", function () {
+describe("Search in project module -viewports", function () {
+  beforeEach(function () {
+    cy.login();
+    addProject_PO.visitProjectModulePage();
+  });
+  it("Should create a new project", function () {
+    const randomGenerateName = getRandomString();
+    addProject_PO.projectInput(randomGenerateName);
+    addProject_PO.clientInput();
+    addProject_PO.createProject();
+  });
+  it("click billable button - viewport and presets", function () {
+    cy.viewport("ipad-mini", "portrait");
     cy.get('[data-cy="toggleBillable"]').click();
     cy.get('[data-cy="toggleBillable"]')
       .invoke("attr", "title")
       .should("eq", "Billable");
   });
-  it("Should logout", function(){
-    cy.get('.h-6 > path').click()
-    cy.url().should('eq', 'https://frontendbootcamp.proshore.eu/accounts/login')
-  } )
-    })
-  
+  it("Should logout", function () {
+    cy.get(".h-6 > path").click();
+    cy.url().should(
+      "eq",
+      "https://frontendbootcamp.proshore.eu/accounts/login"
+    );
+  });
+});
--- cypress/page_objects/AddProject/addProject_PO.js
+++ cypress/page_objects/AddProject/addProject_PO.js
@@ -1,22 +1,20 @@
-class addProject_PO{
-    elements = {
-        projectInput : () => cy.get('[data-cy="addProjectname"]'),
-        clientInput : () =>cy.get('[data-cy="selectClient"]').select("Jane Doe"),
-        createBtn : () => cy.get('[data-cy="newProjectCreateButton"]'),
-        // alertMessage : (txt) =>cy.on(".window:alert", (txt) ),
-    }
-    visitProjectModulePage(){
-      cy.get('[data-cy="projectsSidebarButton"]').contains("Projects").click();
-    }
-    projectInput(project){
-        this.elements.projectInput().type(project)
-    }
-    clientInput(){
-        this.elements.clientInput().select("Jane Doe")
-    }
-    createProject(){
-        this.elements.createBtn().click({force : true})
-    }
-    
+class addProject_PO {
+  elements = {
+    projectInput: () => cy.get('[data-cy="addProjectname"]'),
+    clientInput: () => cy.get('[data-cy="selectClient"]').select("Jane Doe"),
+    createBtn: () => cy.get('[data-cy="newProjectCreateButton"]'),
+  };
+  visitProjectModulePage() {
+    cy.get('[data-cy="projectsSidebarButton"]').contains("Projects").click();
   }
-  module.exports = new addProject_PO ();
+  projectInput(project) {
+    this.elements.projectInput().type(project);
+  }
+  clientInput() {
+    this.elements.clientInput().select("Jane Doe");
+  }
+  createProject() {
+    this.elements.createBtn().click({ force: true });
+  }
+}
+module.exports = new addProject_PO();
