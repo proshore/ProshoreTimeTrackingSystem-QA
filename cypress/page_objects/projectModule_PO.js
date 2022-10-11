f4a2d512196f8b4aec17f00b8fe16bbb296be3b4
mahima <mahimabh93gmail.com>
2022-10-11 10:38:49 +0545

    negative cases

---
+++ cypress/page_objects/projectModule_PO.js
@@ -0,0 +1,22 @@
+class projectModule_PO{
+    elements = {
+        projectInput : () => cy.get('[data-cy="addProjectname"]'),
+        clientInput : () =>cy.get('[data-cy="selectClient"]').select("Jane Doe"),
+        createBtn : () => cy.get('[data-cy="newProjectCreateButton"]'),
+        // alertMessage : (txt) =>cy.on(".window:alert", (txt) ),
+    }
+    visitProjectModulePage(){
+      cy.get('[data-cy="projectsSidebarButton"]').contains("Projects").click();
+    }
+    projectInput(project){
+        this.elements.projectInput().type(project)
+    }
+    clientInput(){
+        this.elements.clientInput().select("Jane Doe")
+    }
+    createProject(){
+        this.elements.createBtn().click({force : true})
+    }
+    
+  }
+  module.exports = new projectModule_PO ();
